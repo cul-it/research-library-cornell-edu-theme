0331b2c751a8366bce9fd3f4eb48676f03d8c0e1
Melissa Wallace <mhk33@cornell.edu>
2015-08-20 14:10:55 -0400

    add piwik code

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -72,5 +72,21 @@
       jQuery( '.cul-icon-chevron' ).before( '<i class="fa fa-chevron-right"></i>' );
     });
   </script>
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//webanalytics.library.cornell.edu/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 509]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//webanalytics.library.cornell.edu/piwik.php?idsite=509" style="border:0;" alt="" /></p></noscript>
+  <!-- End Piwik Code -->
+
 </body>
 </html>
